49fdc6d99475339fe1aec7b28c12bd5361fbbf44
栗原菜奈 <nana.kurihara@rakus-partners.co.jp>
2025-07-11 19:59:14 +0900

    コミット

---
+++ src/jp/co/rakuspartners/java/basic/exam11/Ex47.java
@@ -0,0 +1,13 @@
+package jp.co.rakuspartners.java.basic.exam11;
+
+public class Ex47 {
+    public static void main(String[] args) {
+        
+     System.out.println(average(79, 80, 100));
+    }
+
+    static double average(int num1,int num2,int num3){
+        return (num1 + num2 + num3)/3.0;
+    }
+
+}
